378d4343defaa02508c9bab98d654c21de864ce5
kalilovm <marlenkalilov86@gmail.com>
2023-12-22 16:40:26 +0100

    Saving tokens to cookies
    Added:
    - Saving cookies from backend to cookies
    - Parse cookies
    - Added exp date

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -1,4 +1,25 @@
 import {NextRequest, NextResponse} from "next/server";
+import {cookies} from "next/headers";
+import {jwtDecode} from "jwt-decode"
+
+type LoginResponse = {
+  username: string;
+  tokens: {
+    refresh: string;
+    access: string;
+  };
+  avatar: string;
+  email: string;
+}
+
+type tokenType = {
+  token_type: "access" | "refresh";
+  exp: number,
+  iat: number,
+  jti: string,
+  // TODO: user_id will change to uuid or other one
+  user_id: number,
+}
 
 export async function POST(req: NextRequest) {
   const {username, password} = await req.json(); // TODO: typing for this
@@ -9,8 +30,15 @@ export async function POST(req: NextRequest) {
     },
     body: JSON.stringify({username, password})
   })
-  const res = await response.json();
-  // TODO: add access and refresh tokens to cookies
+  const res: LoginResponse = await response.json();
+  const accessTokenPayload = jwtDecode<tokenType>(res.tokens.access);
+  const refreshTokenPayload = jwtDecode<tokenType>(res.tokens.refresh);
+  // TODO: add path and secure to cookies
+  cookies().set(accessTokenPayload.token_type, res.tokens.access, {expires: new Date(accessTokenPayload.exp * 1000), httpOnly: true, sameSite: "strict"});
+  cookies().set(refreshTokenPayload.token_type, res.tokens.refresh, {expires: new Date(refreshTokenPayload.exp * 1000), httpOnly: true, sameSite: "strict"});
+  // decode header by passing in options (useful for when you need `kid` to verify a JWT):
+  // const decodedHeader = jwtDecode(token, { header: true });
+
   return NextResponse.json(res, {
     status: response.status,
     statusText: response.statusText
--- app/auth/signin/models.ts
+++ app/auth/signin/models.ts
@@ -16,6 +16,7 @@ export interface User {
   id: string;
   username: string;
   email: string;
+  avatar: string;
   tokens: Tokens;
 }
 
